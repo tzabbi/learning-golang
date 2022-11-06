d186b4a1bde365b6f1802ac9a7265b00863f74d4
tzabbi <t.zaspel@gmx.de>
2022-11-06 17:41:23 +0100

    add password generation logic

--- password-generator/src/password-generator/main.go
+++ password-generator/src/password-generator/main.go
@@ -1,3 +1,17 @@
+
+func generatePassword(passwordLength int) (password string) {
+
+	// TODO: generate always another string not the same
+	charSet := lowerCharSet + upperCharSet + specialCharSet + numberSet
+	characterCount := len(charSet)
+
+	for i := 0; i < passwordLength; i++ {
+		randomPosition := rand.Intn(characterCount)
+		randomChar := string(charSet[randomPosition])
+		password = password + randomChar
+	}
+	return
+}
 func checkUserInput() (userInputhLength int) {
 	var userInput string
 	fmt.Print("Please enter the length of your password: ")
